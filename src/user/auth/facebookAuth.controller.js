24ff22aa1180e772f1127f9ef2ea10abeeed5473
orefash <orefash@gmail.com>
2022-11-12 05:41:46 +0100

    add passport jwt and facebook

---
+++ src/user/auth/facebookAuth.controller.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const passport = require("passport");
+
+
+function authRoutes() {
+    const router = express.Router();
+
+    router.get(
+        '/facebook',
+        passport.authenticate('facebook', {
+            scope: ['public_profile', 'email'],
+        }),
+    );
+
+    const clientUrl = process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL_PROD : process.env.CLIENT_URL_DEV;
+
+
+    router.get(
+        '/facebook/callback',
+        passport.authenticate('facebook', {
+            failureRedirect: '/',
+            session: false,
+        }),
+        (req, res) => {
+            console.log("in fb: ", req.user);
+            const token = req.user.generateJWT();
+            res.cookie('x-auth-cookie', token);
+            res.redirect(clientUrl);
+        },
+    );
+
+
+    return router;
+
+}
+
+
+
+module.exports.authRoutes = authRoutes
