5b299e03b2b7d1053d69e40ea3b5bd805f8013c8
orefash <orefash@gmail.com>
2022-12-20 11:48:40 +0100

    added redis prod url

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,5 +1,13 @@
 const Redis = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL);
+const isProduction = process.env.NODE_ENV === 'production';
+
+let redis_url = null;
+if(isProduction)
+    redis_url = process.env.REDIS_URL_PROD
+else
+    redis_url = process.env.REDIS_URL_DEV
+
+const redis = new Redis(redis_url);
 
 module.exports = redis;
